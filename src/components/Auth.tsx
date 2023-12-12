0c81b3e3d131c7c2a31cff3e016b4675a2d970b5
Nirmal Bhandari <bhdnirmal99@gmail.com>
2023-12-12 14:36:27 +0000

    first commit

---
+++ src/components/Auth.tsx
@@ -0,0 +1,13 @@
+import {Box, Typography} from "@mui/material";
+
+const Auth = () => {
+  return (
+    <>
+        <Box display="flex" flexDirection="column">
+            <Typography> This is a form for authentication</Typography>
+        </Box>
+    </>
+  )
+}
+
+export default Auth
---
+++ src/utils/fetchFromAPI.ts
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+
+// These functions aren't exaclty correct and need to be updated to work with the API
+// const fetchFromAPI = async (endpoint: string) => {
+//   const { data } = await axios.get(endpoint);
+//   return data;
+// };
+
+// const postsToAPI = async (endpoint: string, data: any) => {
+//   const { data: post } = await axios.post(endpoint, data);
+//   return post;
+// }
+
+export {}
